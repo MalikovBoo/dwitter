066e79f26a7389186dc70fbba3b1ad6060cd1a9b
malikovboo <malikovboo@gmail.com>
2023-01-18 05:55:26 +0600

    added follow-unfollow buttons and dweets model

--- dwitter/models.py
+++ dwitter/models.py
@@ -29,3 +29,19 @@ class Profile(models.Model):
         return self.user.username
 
 
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.DO_NOTHING,
+        related_name='dweets'
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return (
+            f"{self.user} "
+            f"({self.created_at:%Y-%m-%d %H:%M}): "
+            f"{self.body[:30]}..."
+        )
+
