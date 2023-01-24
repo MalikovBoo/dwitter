d8bddb288903dd7746e417d4b6507e7b115bcdb4
malikovboo <malikovboo@gmail.com>
2023-01-24 22:19:45 +0600

    Deploy to Heroku

--- social/settings.py
+++ social/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -130,3 +132,7 @@ LOGOUT_REDIRECT_URL = "dwitter:dashboard"
 
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
+
+ALLOWED_HOSTS = ['your-app-name.herokuapp.com', '127.0.0.1']
